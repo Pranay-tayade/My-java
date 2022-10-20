2565614d6be2b1f03623203ae4ca6bc8c0f51ff1
Pranay-tayade <113009194+Pranay-tayade@users.noreply.github.com>
2022-10-20 15:15:03 +0530

    Add files via upload

---
+++ LabPractical/AscendingDessendingOrder.java
@@ -0,0 +1,39 @@
+package javapractical;
+
+import java.util.*;
+
+
+public class AscendingDessendingOrder {
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number For Array");
+		int m = sc.nextInt();
+		Integer array[]=new Integer[m];
+		
+		System.out.println("Enter elements ");
+		
+		for(int i=0;i<m;i++)
+		{
+			array[i] = sc.nextInt();
+		}
+		
+		System.out.println("Original array" + Arrays.toString(array));
+		
+		Arrays.sort(array,Collections.reverseOrder());
+		System.out.println("Descending array is" + Arrays.toString(array));
+		
+		for(int i=0;i>m;i++)
+		{
+			array[i] = sc.nextInt();
+		}
+		
+		System.out.println("Original array" + Arrays.toString(array));
+		
+		Arrays.sort(array);
+		System.out.println("Descending array is" + Arrays.toString(array));
+		sc.close();
+	}
+
+}
---
+++ LabPractical/EvenOrder.java
@@ -0,0 +1,42 @@
+package javapractical;
+
+import java.util.*;
+
+public class EvenOrder {
+
+	
+	public static void main(String args[])
+	{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the number of element in array");
+			
+			int a = sc.nextInt();
+				
+			
+			Integer array[]=new Integer[a];
+			
+			System.out.println("Enter elements ");
+			{
+			for(int i=0;i<a;i++)
+			{
+				int n = sc.nextInt();
+				if(n%2==0) {
+				array[i] = n;
+				}
+				else {
+					array[i]=0;
+				}
+			}
+			sc.close();
+			System.out.println("Original array" + Arrays.toString(array));
+		
+			Arrays.sort(array);
+			for(int i=0;i<a;i++) {
+			if(array[i]%2==0)
+			{
+			System.out.println("Ascending array is: " +(array[i]));
+			}
+			}
+			}
+}
+}
